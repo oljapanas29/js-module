11e3d1a2ce3ae90974f14e33afadc277ffb22248
oljapanas29 <oljapanas29@gmail.com>
2021-11-30 21:41:11 +0200

    homework8

---
+++ homework8/hw8.js
@@ -0,0 +1,35 @@
+// - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// -- отримує текст з параграфа з id "content"
+// const contentParagraph = document.getElementById('content');
+// console.log(contentParagraph.textContent);
+
+// -- отримує текст з блоку з id "rules"
+// const rulesBlock = document.getElementById('rules');
+// console.log(rulesBlock.textContent);
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// contentParagraph.textContent = 'content for example';
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// rulesBlock.textContent = 'rules for example';
+
+// -- змініть кожному елементу колір фону на червоний
+// let children = document.body.children;
+// for (const child of children) {
+//     child.style.background = 'red'
+// }
+
+// -- змініть кожному елементу колір тексту на синій
+// let children = document.body.children;
+// for (const child of children) {
+//     child.style.color = 'blue'
+// }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// console.log(rulesBlock.classList);
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// const fcRulesElArray = document.getElementsByClassName('fc_rules');
+// for(const element of fcRulesElArray) {
+//     element.style.color = 'red';
+// };
